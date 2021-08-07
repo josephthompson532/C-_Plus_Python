c0d66ae1c5b6e18f594f8a0db18805036ef479b6
josephthompson532 <66881241+josephthompson532@users.noreply.github.com>
2021-08-07 00:30:00 -0700

    initial upload

---
+++ Module6_Activity.vcxproj/Release/PythonCode.py
@@ -0,0 +1,14 @@
+import re
+import string
+
+def MultiplicationTable(num):
+    for x in range(1, 10):
+        print("{} X {} = {}".format(num, x, num*2))
+    return 1
+
+def DoubleValue(num):
+    return num * 2
+
+
+
+    
